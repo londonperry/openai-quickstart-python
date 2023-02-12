393b5892273bc43fb148195f350457213ec76510
London Perry <londonperry23@gmail.com>
2023-02-12 00:43:07 -0700

    initial commit

--- app.py
+++ app.py
@@ -1,35 +1,42 @@
 import os
-
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+from bs4 import BeautifulSoup
+import requests
+from flask import Flask, render_template, request, redirect, url_for
+import urllib.parse as urlparse
+
+def summarize_url(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    p_tags = soup.find_all("p")
+    text = '\n'.join([p.text for p in p_tags])
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"Please summarize the contents of the URL: {url}\n\n{text}",
+        temperature=0.5,
+        max_tokens=200,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+    return response.choices[0].text
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+        url = request.form["url"]
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        parsed_url = urlparse.urlparse(url)
+        domain = parsed_url.hostname.split(".")[-2].upper()
+        title = soup.find("h1").text.strip()
+        summary = summarize_url(url)
+        response = f"Source: {domain}\n.Article Title: {title}\n\n.{summary}"
+        return redirect(url_for("index", result=response))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
